db36e6aace43d0ddcd93fff5fdc71b41d0136945
Mirko Cukich <mirkocukich@gmail.com>
2019-08-03 00:03:30 -0500

    Program Complete

--- Swift Number Formatter/ViewController.swift
+++ Swift Number Formatter/ViewController.swift
@@ -14,19 +14,28 @@ class ViewController: UIViewController {
     // counter label
     @IBOutlet weak var counterLabel: UILabel!
     
+    // number formatter
+    let formatter = NumberFormatter()
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        // no decimals just integers
+        formatter.allowsFloats = false
     }
     
+    // adds 1 number to current count
     @IBAction func tapAwayButton(_ sender: UIButton) {
-        
+        // number out of string
+        if let numberFromLabel = formatter.number(from: counterLabel.text!) {
+            // increments number in label by 1
+            counterLabel.text = "\(numberFromLabel.intValue + 1)"
+        }
         
     }
-    
+    // resets count to 0
     @IBAction func resetButton(_ sender: Any) {
-        
+        counterLabel.text = "\(0)"
         
     }
     
